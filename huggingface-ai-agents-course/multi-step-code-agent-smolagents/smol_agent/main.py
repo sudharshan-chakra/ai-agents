99547ae2fabdd604f8b7fb90c1e64fcea29f7c59
Sudharshan <sudharshan.chakravarthy@gurucul.com>
2025-10-17 18:48:56 -0700

    testbed and basic agents for smolagents

---
+++ huggingface-ai-agents-course/multi-step-code-agent-smolagents/smol_agent/main.py
@@ -0,0 +1,52 @@
+import os
+from dotenv import load_dotenv
+from smolagents import CodeAgent, WebSearchTool, InferenceClientModel
+from smolagents import tool
+import datetime
+
+# Load the env variables
+load_dotenv()
+hf_token = os.getenv("HUGGINGFACE_READ_KEY")
+
+@tool
+def suggest_menu(occasion: str) -> str:
+    """
+    Suggests a menu based on the occasion.
+    Args:
+        occasion (str): The type of occasion for the party. Allowed values are:
+                        - "casual": Menu for casual party.
+                        - "formal": Menu for formal party.
+                        - "superhero": Menu for superhero party.
+                        - "custom": Custom menu.
+
+    Returns:
+        str: Menu suggestion
+    """
+    if occasion == "casual":
+        return "Pizza, snacks, and drinks."
+    elif occasion == "formal":
+        return "3-course dinner with wine and dessert"
+    elif occasion == "superhero":
+        return "Buffet with high-energy and healthy food."
+    else:
+        return "Custom menu for the butler"
+
+agent = CodeAgent(tools=[suggest_menu,WebSearchTool()], model=InferenceClientModel(), additional_authorized_imports=['datetime'])
+
+# Step 1: Music recommendations
+# agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
+
+# Step 2: Menu preparation
+# agent.run("Prepare a formal menu for the party.")
+
+agent.run(
+    """
+    Alfred needs to prepare for the party. Here are the tasks:
+    1. Prepare the drinks - 30 minutes
+    2. Decorate the mansion - 60 minutes
+    3. Set up the menu - 45 minutes
+    3. Prepare the music and playlist - 45 minutes
+
+    If we start right now, at what time will the party be ready?
+    """
+)
